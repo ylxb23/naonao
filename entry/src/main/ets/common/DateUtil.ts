dda7ccb9f7acc383784c6cabb48579c1ee863cc8
ylxb23 <ylxb23@163.com>
2024-05-23 00:00:01 +0800

    抽象优化02

---
+++ entry/src/main/ets/common/DateUtil.ts
@@ -0,0 +1,35 @@
+
+const DAY_OF_TIME_MILLISECOND = 24 * 3600 * 1000;
+
+export function nextAnniversaryDays(date: Date, _of?: Date | null): number {
+  if(_of == null) {
+    _of = new Date()
+  }
+  let _date = new Date(date)
+
+  _date.setFullYear(_of.getFullYear())
+  if (_of.getTime() > _date.getTime()) {
+    // 今年生日已过，年份+1，计算到明年的天数
+    _date.setFullYear(_of.getFullYear() + 1)
+  }
+  return Math.ceil((_date.getTime() - _of.getTime()) / DAY_OF_TIME_MILLISECOND)
+}
+
+
+export function lastDaysOfDate(date: Date): number {
+  const now = new Date();
+  return Math.ceil((date.getTime() - now.getTime()) / DAY_OF_TIME_MILLISECOND)
+}
+
+export function daysPassedOfDate(date: Date): number {
+  const now = new Date();
+  return Math.floor((now.getTime() - date.getTime()) / DAY_OF_TIME_MILLISECOND)
+}
+
+export function formatDateTime(date: Date): string {
+  return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+}
+
+export function formatDate(date: Date): string {
+  return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
+}
--- entry/src/main/ets/viewmodel/card/Card.ts
+++ entry/src/main/ets/viewmodel/card/Card.ts
@@ -1,10 +1,24 @@
 
+import { nextAnniversaryDays } from '../../common/DateUtil'
+export const DISPLAY_BIG_WIDTH = 360
+export const DISPLAY_BIG_HEIGHT = 150
+export const DISPLAY_BIG_LEFT_INFO_WIDTH = 130
+export const DISPLAY_BIG_LEFT_INFO_HEIGHT = DISPLAY_BIG_LEFT_INFO_WIDTH
+export const DISPLAY_BIG_RIGHT_LIST_WIDTH = 200
+export const DISPLAY_BIG_RIGHT_LIST_HEIGHT = DISPLAY_BIG_LEFT_INFO_HEIGHT
+
+export const DISPLAY_MIDDLE_WIDTH = DISPLAY_BIG_HEIGHT
+export const DISPLAY_MIDDLE_HEIGHT = DISPLAY_MIDDLE_WIDTH
+
+export const DISPLAY_SMALL_WIDTH = DISPLAY_MIDDLE_WIDTH
+export const DISPLAY_SMALL_HEIGHT = DISPLAY_SMALL_WIDTH / 3
+
 /**
  * 所有卡片共有的属性
  */
 abstract class AbsCard {
-  public readonly type: CardTypeEnum
-  public size: SizeEnum
+  public readonly type?: CardTypeEnum
+  public size?: SizeEnum
 
   constructor(t: CardTypeEnum, s?: SizeEnum) {
     this.type = t
@@ -21,7 +35,6 @@ export class AnniversaryCardItem extends AbsCard {
   title: string
   date: Date
 
-
   constructor(t: string, d: Date) {
     super(CardTypeEnum.Anniversary)
     this.title = t
@@ -29,19 +42,28 @@ export class AnniversaryCardItem extends AbsCard {
   }
 }
 
-export interface NamedDateItem {
+export class NamedDateItem {
   name: string
   date: Date
   /**
-   * 计算得出：
+   * 计算得出，每种计时规则使用不同的算法得出，用于排序
    * 已经过去的天数
    */
-  // days: number
+  // private days: number
   /**
    * 计算得出：
    * 剩余时间的详细计数：年，月，日，时，分，秒
    */
   // detail: [number, number, number, number, number, number]
+
+  public nextAnniversaryCountdownDays?(_of?: Date): number {
+    return nextAnniversaryDays(this.date, _of)
+  }
+  public compareAnniversaryDayCountdownTo?(_that: NamedDateItem, _of?: Date | null): number {
+    let thisSortVal = this.nextAnniversaryCountdownDays(_of)
+    let thatSortVal = _that.nextAnniversaryCountdownDays(_of)
+    return thisSortVal - thatSortVal
+  }
 }
 
 /**
@@ -83,6 +105,24 @@ export class CountdownListCardItem extends AbsCard {
     super(CardTypeEnum.CountdownList)
     this.title = t
     this.list = l
+  }
+
+  /**
+   * 获取纪念日最近的一个倒计时时间项
+   * @returns
+   */
+  public getNextAnniversaryDayCountdownItem?(): NamedDateItem {
+    if(this.list == undefined || this.list.length == 0) {
+      return undefined
+    }
+    let now = new Date()
+    let sorted = this.list.sort((a, b) => {return a.compareAnniversaryDayCountdownTo(b, now)})
+    return sorted[0]
+  }
+
+  public getSortedAnniversaryDayCountdownList?(): NamedDateItem[] {
+    let now = new Date()
+    return this.list.sort((a, b) => {return a.compareAnniversaryDayCountdownTo(b, now)})
   }
 }
 
